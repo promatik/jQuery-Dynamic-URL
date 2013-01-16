2b050a4bd5ccc44f358d80642d01ddf6f4456bf3
Toni Almeida <promatik@gmail.com>
2013-01-16 03:08:56 +0000

    Support Forum Link Added

--- example/script.js
+++ example/script.js
@@ -73,11 +73,3 @@ $(document).ready(function() {
 		$('html,body').animate({scrollTop: $("#"+id).offset().top-80},'slow');
 	}
 });
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/pt_PT/all.js#xfbml=1&appId=150154111693861";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
